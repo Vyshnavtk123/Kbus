6048a450547c0edc299acaf9d20a90cfd4cb6e80
VYSHNAV T K <vyshnavtk18@gmail.com>
2026-02-19 15:24:01 +0530

    staticfiles commit

--- kbus_project/settings.py
+++ kbus_project/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kbus-1.onrender.com']
+ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'transport.User'
 
